539effbc054d943304123258d90bea6c02b8b230
suntongf@126.com <suntongf@126.com>
2019-08-21 11:21:45 +0800

    基本用法分析完毕

--- app/src/main/java/com/kk/tongfu/databinding/MainActivity.kt
+++ app/src/main/java/com/kk/tongfu/databinding/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.kk.tongfu.databinding
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kk.tongfu.databinding.databinding.ActivityMainBinding
+import com.kk.tongfu.databinding.entity.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.user=User("tongfu",25,"shanghai")
     }
 }
---
+++ app/src/main/java/com/kk/tongfu/databinding/entity/User.kt
@@ -0,0 +1,10 @@
+package com.kk.tongfu.databinding.entity
+
+/**
+ * Created by tongfu
+ * on 2019-08-20
+ * Desc:
+ */
+
+ data class User(val userName:String,val userAge:Int,val userAddress:String) {
+}
